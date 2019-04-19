c05584b4a7bbfad9e6573cc7024fd72aa8cf04f4
Donna <skies09@hotmail.com>
2019-04-19 15:42:56 +0000

    adding carousel, bee links and styles

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Coders without Borders
+# **Coders without Borders**
+--
+This is the project for Hackathon April 2019
+
+## Strategy for the project
+### What the project does
+* This is a bee themed website.
+
+### The need it fulfills
+* 
+
+### Target Audience
+* People inetrested in bees.
+
+# How the website is setup
+---
+The body of the website is set up into
+* Navigation Bar
+* To-Do List
+* Bee links
+* Carousel
+* Poll
+
+* Carousel 
+
+The Carousel was built with HTML, CSS and vanilla JavaScript.
+
+Bugs
--- static/js/script.js
+++ static/js/script.js
@@ -54,3 +54,89 @@
           }
         }
       }
+      
+  // Carousel Code
+  
+var carouselSlide = document.querySelector(".holder");
+var carouselImages = document.querySelectorAll(".holder img"); 
+var nextBtn = document.getElementById("nextBtn");
+var prevBtn = document.getElementById("prevBtn");
+var counter = 1;
+var myInterval;
+var playPause = document.getElementById("playPause");
+
+// start of slideshow:
+
+carouselSlide.style.transform = "translateX(" + (counter * -20) + "%)";
+
+// functions:
+
+function slideRight(){
+  carouselSlide.style.transition = "transform 0.4s ease-in-out"; 
+	counter++;
+	carouselSlide.style.transform = "translateX(" + (counter * -20) + "%)";
+}
+
+function slideLeft(){
+  carouselSlide.style.transition = "transform 0.4s ease-in-out"; 
+	counter--;
+	carouselSlide.style.transform = "translateX(" + (counter * -20) + "%"  
+}
+// button navigation:
+
+nextBtn.addEventListener("click", function(){
+	if(counter >= carouselImages.length -1) return; 
+  slideRight();
+});
+
+
+prevBtn.addEventListener("click", function(){
+	if(counter <=0) return; 
+  slideLeft();
+});
+
+// transitions:
+
+carouselSlide.addEventListener("transitionend", function(){
+  if(counter === 4){ // (total number of slides -1)
+  	carouselSlide.style.transition = "none"; 
+  	counter = 1;
+  	carouselSlide.style.transform = "translateX(" + (counter * -20) + "%";
+  }
+
+  if(counter === 0){
+  	carouselSlide.style.transition = "none"; 
+  	counter = 3; // (total number of slides - 2)
+  	carouselSlide.style.transform = "translateX(" + (counter * -20) + "%";
+  }
+});
+
+// play-pause button functionality:
+
+   function myFunction() { 
+        if(counter >= carouselImages.length -1) return; 
+     slideRight();
+      };
+
+    playPause.addEventListener("change", function(){
+        if(this.checked != true){
+          myInterval=setInterval(myFunction, 3000);
+        } else 
+          clearInterval(myInterval);
+        
+});
+
+// arrow-key navigation:
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            if(counter <=0) return; 
+        slideLeft();
+            break;
+        case 39:
+            if(counter > 3) return;
+        slideRight();
+            break;
+    }
+};
